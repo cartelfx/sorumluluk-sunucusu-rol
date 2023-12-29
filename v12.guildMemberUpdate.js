7fc8f8cc320aefd5ddf27010b3d6eb1c281364f1
cartel <119452317+cartelfx@users.noreply.github.com>
2023-12-29 17:15:17 +0400

    Update v12.guildMemberUpdate.js

--- v12.guildMemberUpdate.js
+++ v12.guildMemberUpdate.js
@@ -1,1 +1,40 @@
 
+const { Client } = require('discord.js');
+const client = new Client();
+
+const anaSunucuID = 'ANA_SUNUCU_ID';
+const sorumlulukSunucuID = 'SORUMLULUK_SUNUCU_ID';
+const staffs = [
+    { rol: 'YETKİLİ SORUMLUSU' },
+    { rol: 'KAYIT SORUMLUSU' },
+    { rol: 'STREAMER SORUMLUSU' },
+    /*   { rol: 'SORUMLU' },
+    { rol: 'SORUMLU' },
+    { rol: 'SORUMLU' },
+    { rol: 'SORUMLU' }
+  */
+];
+
+client.on('guildMemberAdd', (yeniUye) => {
+    const anaSunucu = client.guilds.cache.get(anaSunucuID);
+    const sorumlulukSunucu = client.guilds.cache.get(sorumlulukSunucuID);
+
+    const yeniUyeRoller = yeniUye.roles.cache.map(role => role.name);
+
+    const yetkiKontrolü = staffs.some(staff => yeniUyeRoller.includes(staff.rol));
+
+    if (!yetkiKontrolü) {
+        return;
+    }
+
+    staffs.forEach(staff => {
+        const kontrolEdilecekRolAdi = staff.rol;
+
+        if (yeniUyeRoller.includes(kontrolEdilecekRolAdi)) {
+            const sorumlulukUye = sorumlulukSunucu.members.cache.get(yeniUye.id);
+            if (sorumlulukUye && kontrolEdilecekRolAdi !== '') {
+                sorumlulukUye.kick();
+            }
+        }
+    });
+});
